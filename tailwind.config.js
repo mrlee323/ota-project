067d607f6b281b5aa338058a876872173dca634c
mrlee323 <mrlee3233@gmail.com>
2026-03-01 01:12:41 +0900

    style: Tailwind brand 컬러 팔레트 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
     "./src/ui/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: "#6728E0",
+          50: "#F4EFFE",
+          100: "#E5D9FC",
+          200: "#C9B0F9",
+          300: "#AD87F5",
+          400: "#8B55EF",
+          500: "#6728E0",
+          600: "#5520B8",
+          700: "#431990",
+          800: "#311268",
+          900: "#1F0B40",
+        },
+      },
+    },
   },
   plugins: [],
 };
